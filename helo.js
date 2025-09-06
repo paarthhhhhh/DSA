bb007b7b8089467784be17108e371b6e87fafb9d
Parth Bhattt <parthbhatt1921@gmail.com>
2025-09-06 15:06:55 +0530

    basics of js

---
+++ helo.js
@@ -0,0 +1,11 @@
+console.log("hello");
+
+let arr=["Parth", "Superman", "Batman"];
+
+console.log(arr[1]);
+
+obj={
+    name:"Parth",
+    age:22
+}
+console.log(obj.name, obj.age)
---
+++ if-else.js
@@ -0,0 +1,18 @@
+function multiply(a,b){
+    let mult=a*b;
+    console.log("Here you go bruv :" ,mult)
+}
+multiply(19,12)
+
+function evenodd(num){
+    if(num<1){
+        console.log("Invalid Number");
+    }
+    else if(num%2==0){
+        console.log("Even Number")
+    }
+    else{
+        console.log("Odd Number")
+    }
+}
+evenodd(10)
