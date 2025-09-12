e3c746fb9abb2f213ebc6c4b714f8364b130544c
Parth Bhattt <parthbhatt1921@gmail.com>
2025-09-12 16:06:19 +0530

    practiced patterns

---
+++ pattern1.js
@@ -0,0 +1,8 @@
+let n=4;
+for(i=0;i<n;i++){
+    let row=" ";
+    for(j=0;j<n;j++){
+        row= row +"*";
+    }
+    console.log(row);
+}
---
+++ pattern2.js
@@ -0,0 +1,9 @@
+let n=1;
+for(i=0;i<5;i++){
+    row=" ";
+    for(j=0;j<n;j++){
+        row=row+"*";
+    }
+    n++
+    console.log(row)
+}
