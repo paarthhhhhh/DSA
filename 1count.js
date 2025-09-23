9a5a8100685f2b0f09df1aadf3fa5a3880b51e71
Parth Bhattt <parthbhatt1921@gmail.com>
2025-09-23 23:28:07 +0530

    count numbers

---
+++ 1count.js
@@ -0,0 +1,15 @@
+function countDigits(n){
+    let count = 0;
+    if(n==0){
+        return 1;
+    }
+    n=Math.abs(n);            //Math.absolute changes negative no. to positive
+    while(n>0){
+        n=Math.floor(n/10);   //rounds down the value
+        count++;
+    }
+    return count;             //function hamesha return karega ni toh result undefined hoga 
+}
+let n=-10;
+let result=countDigits(n);
+console.log(result);
